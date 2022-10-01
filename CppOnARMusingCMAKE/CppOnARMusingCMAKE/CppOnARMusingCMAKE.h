e4ea629b8c61086835684dfe201e8a6c0db40075
Vijay <vijaykantibhai@yahoo.in>
2022-10-01 15:15:52 +0000

    cleanup

--- CppOnARMusingCMAKE/CppOnARMusingCMAKE/CppOnARMusingCMAKE.h
+++
@@ -1,8 +0,0 @@
-﻿// CppOnARMusingCMAKE.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
